16610fcb67f6c72205e708b42546bb599d3246b3
Ekaterina Komratova <ekaterina.komratova@gmail.com>
2021-10-31 21:02:01 +0300

    Add files

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 52_483;
+        int oneMile = 20;
+        int summMile = ticketPrice / oneMile;
+        System.out.println("Вам начислено:" + " " + summMile + " " + "миль");
+    }
+
+}
